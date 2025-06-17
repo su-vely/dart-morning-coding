f77e92f4c2b7a5919710ceebbc010a6f59d0dea7
su-vely <dbtngus92@naver.com>
2025-06-17 10:54:16 +0900

    first commit

---
+++ bin/dart_morning_coding.dart
@@ -0,0 +1,5 @@
+import 'package:dart_morning_coding/dart_morning_coding.dart' as dart_morning_coding;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_morning_coding.calculate()}!');
+}
